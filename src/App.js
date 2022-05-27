6f12b5d093461569328f926b30ce733172fa490e
TK6n9 <amuz@amuzui-Macmini.local>
2022-05-27 20:12:27 +0900

    카카오맵

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import KakaoMap from "./KakaoMap";
 
 function App() {
+  const [text, setText] = useState("");
+  const [result, setResult] = useState("제주 카카오");
+  const [count, setCount] = useState(0);
+
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+  const submit = (e) => {
+    setText(text);
+    setResult(text);
+    setText("");
+    setCount(1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="section-1">
+        <KakaoMap value={result} count={count} />
+        <input
+          className="input"
+          onChange={onChange}
+          placeholder="찾고자하는 장소를 입력해주세요 :-)"
+          value={text}
+        ></input>{" "}
+        <br />
+        <button type="submit" className="btn" onClick={submit}>
+          지도 출력하기
+        </button>
+      </div>
+    </>
   );
 }
 
